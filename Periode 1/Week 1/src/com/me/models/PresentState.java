143bf5992f77b98f6c82c1399a41dca39af61c45
rster20002 <rster2002dev@gmail.com>
2020-09-02 17:30:24 +0200

    Opdracht week 1

---
+++ Periode 1/Week 1/src/com/me/models/PresentState.java
@@ -0,0 +1,2 @@
+package com.me.models;public enum PresentState {
+}
---
+++ Periode 1/Week 1/src/com/me/models/Student.java
@@ -0,0 +1,10 @@
+package com.me.models;
+
+public class Student {
+    public String name;
+    public PresentState present;
+
+    public String toString() {
+        return "Student: " + name + ", " + present;
+    }
+}
